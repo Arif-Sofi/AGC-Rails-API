0fca7b9f553ef5a04d45c6602929d027457762d5
Azim Sofi <azimmiskandar@gmail.com>
2025-01-22 22:42:45 +0900

    修正

--- db/migrate/20250120120735_create_characters.rb
+++ db/migrate/20250120120735_create_characters.rb
@@ -1,8 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[8.0]
   def change
     create_table :characters do |t|
-    t.string :name
-    t.references :anime, null: false, foreign_key: true
+      t.string :name
+      t.references :anime, null: false, foreign_key: true
 
       t.timestamps
     end
